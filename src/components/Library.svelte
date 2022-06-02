fa705d71c22ead7ea1b6f9d751703af59052042d
miromatton <miro.matton@student.odisee.be>
2022-06-02 18:53:08 +0200

    added library page

---
+++ src/components/Library.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import GameCard from "./GameCard.svelte";
+  let games;
+
+  onMount(async () => {
+    fetch("http://localhost:8000/games")
+      .then((response) => response.json())
+      .then((data) => {
+        games = data;
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return [];
+      });
+  });
+</script>
+
+<section id="Library">
+  <h1>Games</h1>
+  {#if games}
+    <div class="grid">
+      {#each games as game}
+        <GameCard
+          title={game.title}
+          url={game.img}
+          description={game.description}
+        />
+      {/each}
+    </div>
+  {/if}
+</section>
+
+<style lang="scss">
+  @use "../styles/color";
+
+  h1 {
+    color: color.$white;
+    font-size: 3em;
+    font-weight: 100;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 0.5em;
+  }
+</style>
