b6b3c75458b968eb70de1ab77437b27ae7231a66
miro.matton <miro.matton@student.odisee.be>
2022-06-12 19:22:28 +0200

    :recycle: refactored api controllers

--- controllers/gameController.ts
+++ controllers/gameController.ts
@@ -1,40 +1,16 @@
 import { Context } from "https://deno.land/x/abc@v1.3.3/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-const { DATA_API_KEY, APP_ID } = config();
-
-const BASE_URI = `https://data.mongodb-api.com/app/${APP_ID}/endpoint/data/beta/action`
-const DATA_SOURCE = "MatrixGames"
-const DATABASE = "MatrixGames"
-const COLLECTION = "games"
-
-const options = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "api-key": DATA_API_KEY
-    },
-    body: ""
-};
+import { fetchGame, fetchGames } from "./apiController.ts";
+import { flashGame, downloadGame } from "../logic.ts";
 
 export const getGame = async (ctx: Context) => {
-    const { id } = ctx.params;
     try {
-        const URI = `${BASE_URI}/findOne`;
-        const query = {
-            collection: COLLECTION,
-            database: DATABASE,
-            dataSource: DATA_SOURCE,
-            filter: { title: id }
-        };
-        options.body = JSON.stringify(query);
-        const dataResponse = await fetch(URI, options);
-        const game = await dataResponse.json();
-
+        const { id } = ctx.params;
+        const game = await fetchGame(id);
         if (game) {
             return ctx.json(game, 200);
         } else {
-            return ctx.string('no games found', 404);
+            return ctx.string('no game found', 404);
         }
     } catch (err) {
         console.log(err);
@@ -44,17 +20,7 @@ export const getGame = async (ctx: Context) => {
 
 export const getGames = async (ctx: Context) => {
     try {
-        const URI = `${BASE_URI}/find`;
-        const query = {
-            collection: COLLECTION,
-            database: DATABASE,
-            dataSource: DATA_SOURCE,
-        };
-        options.body = JSON.stringify(query);
-        const dataResponse = await fetch(URI, options);
-        const games = await dataResponse.json();
-        console.log(games);
-
+        const games = await fetchGames();
         if (games) {
             return ctx.json(games, 200);
         } else {
@@ -66,14 +32,30 @@ export const getGames = async (ctx: Context) => {
     }
 };
 
-export const flashGame = async (ctx: Context) => {
-    console.log("flashing ...");
-    const cmd = Deno.run({
-        cmd: ["esptool.py", "--port", "/dev/ttyUSB0", "--baud", "460800", "write_flash", "--flash_size=detect", "0x10000", "./games/esp32Blinking.ino.esp32.bin"],
-        stdout: "piped",
+export const playGame = async (ctx: Context) => {
+    const { game } = ctx.params;
+    const path = "./games/" + game + ".ino.esp32.bin";
 
-    });
+    try {
+        const gameExist = await Deno.open(path);
+        console.log("is game already installed?: ", gameExist)
+        if (gameExist) {
+            let log = await flashGame(game);
+            console.log("flash: ", log);
+        }
 
-    await cmd.status();
-    return ctx.string(new TextDecoder().decode(await cmd.output()), 200);
+    } catch (e) {
+        if (e instanceof Deno.errors.NotFound) {
+            let log = await downloadGame(game);
+            console.log("download: ", log);
+        }
+    }
+}
+
+export const testFlasher = async () => {
+    const tester = "esp32Blinking";
+    const path = "./games/" + tester + ".ino.esp32.bin";
+
+    let log = await flashGame(tester);
+    console.log("tester: ", log);
 }
